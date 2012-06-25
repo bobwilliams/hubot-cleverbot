dd6d32904f3c4d26475cd5925830c8794ce6f139
Bob Williams <bobwilliams.ii@gmail.com>
2012-06-25 14:40:34 -0400

    code refactor to better include cleverbot-node

--- cleverbot.js
+++ cleverbot.js
@@ -1,81 +1,103 @@
-// chat
+// Description:
+//   Allows you to chat with hubot as a proxy to cleverbot
 //
-// chat <dialog> - chat with hubot via cleverbot
+// Dependencies:
+//   None
+//
+//   Includes code from cleverbot-node (https://github.com/fojas/cleverbot-node)
+//
+// Configuration:
+//   None
+//
+// Commands:
+//   hubot chat <dialog>  - returns cleverbot's response to your dialog
+//
+// Author:
+//   Bob Williams - https://github.com/bobwilliams
 
-var crypto = require('crypto');
-var http = require('http');
+var crypto = require('crypto')
+  , http = require('http')
+  , Cleverbot = function(){
+      this.params = Cleverbot.default_params;
+    };
 
-var defaultParams = {
-    'stimulus'         : '' , 'start'      : 'y'    , 'sessionid' : '',
-    'vText8'           : '' , 'vText7'     : ''     , 'vText6' : '',
-    'vText5'           : '' , 'vText4'     : ''     , 'vText3' : '',
-    'vText2'           : '' , 'icognoid'   : 'wsf'  , 'icognocheck' : '',
-    'fno'              : '0', 'prevref'    : ''     , 'emotionaloutput' : '',
-    'emotionalhistory' : '' , 'asbotname'  : ''     , 'ttsvoice' : '',
-    'typing'           : '' , 'lineref'    : ''     , 'sub' : 'Say',
-    'islearning'       : '1', 'cleanslate' : 'false',
+Cleverbot.default_params = {
+  'stimulus'         : '' , 'start'      : 'y'    , 'sessionid' : '',
+  'vText8'           : '' , 'vText7'     : ''     , 'vText6' : '',
+  'vText5'           : '' , 'vText4'     : ''     , 'vText3' : '',
+  'vText2'           : '' , 'icognoid'   : 'wsf'  , 'icognocheck' : '',
+  'fno'              : '0', 'prevref'    : ''     , 'emotionaloutput' : '',
+  'emotionalhistory' : '' , 'asbotname'  : ''     , 'ttsvoice' : '',
+  'typing'           : '' , 'lineref'    : ''     , 'sub' : 'Say',
+  'islearning'       : '1', 'cleanslate' : 'false',
 };
-
-var parserKeys = [
-    'message', 'sessionid', 'logurl', 'vText8',
-    'vText7', 'vText6', 'vText5', 'vText4',
-    'vText3', 'vText2', 'prevref', '',
-    'emotionalhistory', 'ttsLocMP3', 'ttsLocTXT', 'ttsLocTXT3',
-    'ttsText', 'lineref', 'lineURL', 'linePOST',
-    'lineChoices', 'lineChoicesAbbrev', 'typingData', 'divert'
+Cleverbot.parserKeys = [
+  'message', 'sessionid', 'logurl', 'vText8',
+  'vText7', 'vText6', 'vText5', 'vText4',
+  'vText3', 'vText2', 'prevref', '',
+  'emotionalhistory', 'ttsLocMP3', 'ttsLocTXT', 'ttsLocTXT3',
+  'ttsText', 'lineref', 'lineURL', 'linePOST',
+  'lineChoices', 'lineChoicesAbbrev', 'typingData', 'divert'
 ];
-
-var digest = function(body){
-    var hash = crypto.createHash('md5');
-    return hash.update(body).digest('hex');
+Cleverbot.digest = function(body){
+  var m = crypto.createHash('md5');
+  m.update(body)
+  return m.digest('hex');
 };
 
-var encodeParams = function(params){
-    var encodedParams=[];
-    for(param in params){
-        if(params[param] instanceof Array)
-            encodedParams.push(param+"="+encodeURIComponent(params[param].join(",")));
-        else if(params[param] instanceof Object)
-            encodedParams.push(params(params[param]));
-        else
-            encodedParams.push(param+"="+encodeURIComponent(params[param]));
-    }
-    return encodedParams.join("&");
+Cleverbot.encodeParams = function(a1){
+  var u=[];
+  for(x in a1){
+    if(a1[x] instanceof Array)
+      u.push(x+"="+encodeURIComponent(a1[x].join(",")));
+    else if(a1[x] instanceof Object)
+      u.push(params(a1[x]));
+    else
+      u.push(x+"="+encodeURIComponent(a1[x]));
+  }
+  return u.join("&");
 };
 
-var say = function(message, callback){
-    var query = defaultParams;
-    query.stimulus = message;
-    query.icognocheck = digest(encodeParams(query).substring(9,29));
+Cleverbot.prototype = {
+
+  write : function(message,callback){
+    var clever = this;
+    body = this.params;
+    body.stimulus = message;
+    body.icognocheck = Cleverbot.digest(Cleverbot.encodeParams(body).substring(9,29));
     var options = {
-        host: 'www.cleverbot.com',
-        port: 80,
-        path: '/webservicemin',
-        method: 'POST',
-        headers: {
+      host: 'www.cleverbot.com',
+      port: 80,
+      path: '/webservicemin',
+      method: 'POST',
+      headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': encodeParams(query).length
-        }
+        'Content-Length': Cleverbot.encodeParams(body).length
+      }
     };
     var req = http.request(options, function(res) {
-        var cb = callback || function(){};
-        res.on('data', function(chunk) {
-            var chunk_data = chunk.toString().split("\r")
-            var responseHash = {};
-            for(var i = 0, iLen = chunk_data.length;i<iLen;i++){
-                query[parserKeys[i]] = responseHash[parserKeys[i]] = chunk_data[i];
-            }
-            cb(responseHash['message']);
-        });
-    });
-    req.write(encodeParams(query));
+      var cb = callback || function(){};
+      res.on('data', function(chunk) {
+        var chunk_data = chunk.toString().split("\r")
+          , responseHash = {};
+        for(var i = 0, iLen = chunk_data.length;i<iLen;i++){
+          clever.params[Cleverbot.parserKeys[i]] = responseHash[Cleverbot.parserKeys[i]] = chunk_data[i];
+        }
+        cb(responseHash);
+      });
+     });
+    req.write(Cleverbot.encodeParams(body));
     req.end();
+  }
 };
+
+//module.exports = Cleverbot;
 
 module.exports = function(robot) {
     robot.respond(/chat (.*)/i, function(msg) {
-        say(msg.match[1], function(reply){
-            msg.send(reply);
+        var cleverbot = new Cleverbot();
+        cleverbot.write(msg.match[1], function(answer){
+            msg.send(answer['message']);
         });
     });
 };
